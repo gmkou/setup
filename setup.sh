b3b5d9ff2be1cd03581ba1cc944b70da43eb77cf
Nagano Kosuke <gm_kou@gmail.com>
2022-02-10 23:14:18 +0900

    Rmove source to continue script running.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 /bin/bash -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 cat zshrc >> ~/.zshrc
-source ~/.zshrc
 
 # BREW
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
